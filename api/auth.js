2d5071954c2deab39776a9a95e13e6cc3331cd11
tradingrobotea <111680273+tradingrobotea@users.noreply.github.com>
2026-05-13 08:25:16 +0800

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,12 +1,21 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  const code = url.searchParams.get("code");
+export default {
+  async fetch(request) {
 
-  const validCodes = ["123456", "ABC123"];
+    const url = new URL(request.url);
 
-  if (validCodes.includes(code)) {
-    return new Response("OK");
+    if (url.pathname === "/api/auth") {
+
+      const code = url.searchParams.get("code");
+
+      const validCodes = ["123456"];
+
+      if (validCodes.includes(code)) {
+        return new Response("OK");
+      }
+
+      return new Response("FAIL", { status: 401 });
+    }
+
+    return new Response("Not Found", { status: 404 });
   }
-
-  return new Response("FAIL", { status: 401 });
-}
+};
